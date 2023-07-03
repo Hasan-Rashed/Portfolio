47b8e3a9bdbdc974daa91ea4e13ec6e98d764c00
Hasan Rashed <hasanrashed537@gmail.com>
2023-07-03 12:37:13 +0600

    Register Functionality

---
+++ frontend/src/components/SignUpModal.jsx
@@ -0,0 +1,143 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useRegisterMutation } from '../slices/usersApiSlice';
+import { setCredentials } from '../slices/authSlice';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+
+import { WaveLoading } from 'react-loadingg';
+
+import { toast } from 'react-toastify';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function ModalLogin() {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [register, { isLoading }] = useRegisterMutation();
+
+
+    // get user info from redux store
+  const { userInfo } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if(userInfo){
+      navigate('/profile');
+    }
+  }, [navigate, userInfo]);
+
+  
+  // form submit handler
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    if(password !== confirmPassword){
+      toast.error('Passwords do not match');
+    }else{
+      try{
+        const res = await register({name, email, password }).unwrap();
+        dispatch(setCredentials({...res}));
+        navigate('/');
+
+        toast.success('Logged in successfully');
+      }catch(err){
+        toast.error(err?.data?.message || err.error);
+      }
+    }
+  }
+
+
+  // button click handler
+  const handleButtonClick = () => {
+    // setEmail('');
+    // setPassword('');
+  };
+  
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return (
+    
+    <div>
+      <button className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium' onClick={handleOpen}><AppRegistrationIcon/> SIGN UP</button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            timeout: 500,
+          },
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+
+          <div className="relative w-full max-w-md max-h-full">
+                            
+            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <div className="px-6 py-6 lg:px-8">
+                    <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white text-center">Sign up</h3>
+
+                      {/* loading component */}
+                    { isLoading && <WaveLoading color='#0080FF' size='large' />}
+
+
+                    <form onSubmit={submitHandler} className="space-y-6" action="#">
+                        <div>
+                            <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                            <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Enter name" value={name} onChange={(e) => setName(e.target.value)} required />
+                        </div>
+
+                        <div>
+                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Enter email address" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                        </div>
+
+                        <div>
+                            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                            <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                        </div>
+                        
+
+                        <div>
+                            <label htmlFor="confirmPassword" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
+                            <input type="password" name="confirmPassword" id="confirmPassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder="Confirm password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                        </div>
+
+                        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleButtonClick} >Sign Up</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
