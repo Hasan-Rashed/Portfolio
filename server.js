a0d4f95b8cbf5e25084ba53ca302914b0fde75ab
Hasan Rashed <hasanrashed537@gmail.com>
2023-07-11 21:06:23 +0600

    server js moved from backend directory to root

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ import express  from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import cookieParser from 'cookie-parser';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import connectDB from './config/db.js';
+import { notFound, errorHandler } from './backend/middleware/errorMiddleware.js';
+import connectDB from './backend/config/db.js';
 const port = process.env.PORT || 5000  ;
-import userRoutes from './routes/userRoutes.js';
-import projectRoutes from './routes/projectRoutes.js';
+import userRoutes from './backend/routes/userRoutes.js';
+import projectRoutes from './backend/routes/projectRoutes.js';
 
 
 connectDB();
