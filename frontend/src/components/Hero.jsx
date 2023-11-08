a71c30e62d6384ee7835b6c89953be9ba7bf1a5f
Hasan Rashed <hasanrashed537@gmail.com>
2023-11-08 19:45:09 +0600

    Typewriter changed to frontend focused

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,15 +4,16 @@ import developer from '../assets/developer activity.svg'
 
 const Hero = () => {
   return (
-    <div className=' bg-slate-200 select-none' >
-        <div className="container px-3 mx-auto py-10 text-center items-center justify-center sm:h-80vh md:h-60vh lg:h-40vh xl:h-20vh">
-                <h1 className="text-2xl md:text-4xl font-bold py-5 text-gray-800 uppercase">I'm <span className='text-blue-500'>Kamrul Hasan Rashed</span></h1>
+    <div className='select-none bg-slate-200' >
+        <div className="container items-center justify-center px-3 py-10 mx-auto text-center sm:h-80vh md:h-60vh lg:h-40vh xl:h-20vh">
+                <h1 className="py-5 text-2xl font-bold text-gray-800 uppercase md:text-4xl">I'm <span className='text-blue-500'>Kamrul Hasan Rashed</span></h1>
         
-                <h2 className='text-xl md:text-3xl py-3 font-bold'>
+                <h2 className='py-3 text-xl font-bold md:text-3xl'>
                     <span className='text-gray-800'>A
                     <strong className='text-blue-500'>
                         <Typewriter
-                        words={[' Fullstack Web Application', ' Frontend Web', ' Backend Web']}
+                        // words={[' Fullstack Web Application', ' Frontend Web', ' Backend Web']}
+                        words={[' Frontend Focused Fullstack Web']}
                         loop={1000}
                         cursor
                         cursorStyle='_'
@@ -26,9 +27,9 @@ const Hero = () => {
                 </h2>
 
 
-                <div className="grid grid-cols-1 md:grid-cols-2 py-5 px-3 justify-center items-center">
+                <div className="grid items-center justify-center grid-cols-1 px-3 py-5 md:grid-cols-2">
                     <div>
-                        <p className='leading-7 text-md md:text-xl opacity-50 hover:opacity-100 duration-75 transform hover:scale-105 transition ease-linear text-gray-800'>
+                        <p className='leading-7 text-gray-800 transition duration-75 ease-linear transform opacity-50 text-md md:text-xl hover:opacity-100 hover:scale-105'>
                         &#8221;I am committed to perform my work with full of attention and honesty. I always think of myself as a student and want to adopt and spread more and more knowledge so that it can be helpful for mankind.&#8221;
                         </p>
                     </div>
